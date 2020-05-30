f3b30430e00c9531b3dbfc586de3b06e86f6e9d2
Hristiyana <hpmalinova@github.com>
2020-05-30 14:43:08 +0300

    Thubnail works

--- share_with_me/drive/models.py
+++ share_with_me/drive/models.py
@@ -1,14 +1,7 @@
 import os
 from django.db import models
-from django.conf import settings
 from django.utils.safestring import mark_safe
-
-# the settings for MEDIA_ROOT and MEDIA_URL come from the project settings
-# but could be overridden in the model
-# MEDIA_ROOT = '/home/<user>/project/imgproject/media_cdn'
-# MEDIA_URL = '/media'
-
-MEDIA_ROOT = 'media'
+from share_with_me.settings import MEDIA_ROOT, MEDIA_URL
 
 
 # Create your models here.
@@ -18,14 +11,16 @@ class Image(models.Model):
     image = models.ImageField(upload_to=MEDIA_ROOT, blank=True)
 
     def url(self):
-        return os.path.join(settings.MEDIA_URL, os.path.basename(str(self.image)))
+        image_name = os.path.basename(str(self.image))
+        a = os.path.join(MEDIA_URL, image_name)
+        print('AAAAAAAAAAAAAAAAAAAAAAA', a)
+        return a
 
     def image_tag(self):
         # used in the admin site model as a "thumbnail"
         return mark_safe('<img src="{}" width="150" height="150" />'.format(self.url()))
     image_tag.short_description = 'Image'
     image_tag.allow_tags = True
-
 
     def __str__(self):
         return 'Image idk'
