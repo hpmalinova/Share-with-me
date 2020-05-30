ce16c5c80aa7524b1875dffef106f53068e5cb72
Hristiyana <hpmalinova@github.com>
2020-05-30 14:18:43 +0300

    try1

--- share_with_me/drive/admin.py
+++ share_with_me/drive/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
-from drive.models import Picture
-from django.utils.html import mark_safe
+from drive.models import Image
 
 
-@admin.register(Picture)
-class PictureAdmin(admin.ModelAdmin):
-    list_display = ('name', 'data',)
-    readonly_fields = ["data_image"]
-
-    def data_image(self, obj):
-        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-            url=obj.data.url,
-            width=obj.data.width,
-            height=obj.data.height
-        ))
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    # explicitly reference fields to be shown, note image_tag is read-only
+    list_display = ('image_tag', 'description', 'image',)
+    readonly_fields = ('image_tag',)
--- share_with_me/drive/models.py
+++ share_with_me/drive/models.py
@@ -1,22 +1,31 @@
-from django.core.files.storage import FileSystemStorage
+import os
 from django.db import models
-# from django.utils.html import mark_safe
+from django.conf import settings
+from django.utils.safestring import mark_safe
+
+# the settings for MEDIA_ROOT and MEDIA_URL come from the project settings
+# but could be overridden in the model
+# MEDIA_ROOT = '/home/<user>/project/imgproject/media_cdn'
+# MEDIA_URL = '/media'
+
+MEDIA_ROOT = 'media'
 
 
-fs = FileSystemStorage(location='/media/shared_files')
+# Create your models here.
+class Image(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to=MEDIA_ROOT, blank=True)
 
+    def url(self):
+        return os.path.join(settings.MEDIA_URL, os.path.basename(str(self.image)))
 
-class Picture(models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    data = models.ImageField(storage=fs)
+    def image_tag(self):
+        # used in the admin site model as a "thumbnail"
+        return mark_safe('<img src="{}" width="150" height="150" />'.format(self.url()))
+    image_tag.short_description = 'Image'
+    image_tag.allow_tags = True
+
 
     def __str__(self):
-        return self.name
-
-
-'''
-    def image_tag(self):
-        return mark_safe('<img src="/media/shared_files/%s" width="150" height="150" />' % (self.name))
-
-    image_tag.short_description = 'Image'
-'''
+        return 'Image idk'
