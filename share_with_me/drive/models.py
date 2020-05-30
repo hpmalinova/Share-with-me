6f2d0a45ed36dfb7d8e898725dc483f6d019d293
Hristiyana <hpmalinova@github.com>
2020-05-30 15:12:07 +0300

    Fix image_tag + repr of image

--- share_with_me/drive/models.py
+++ share_with_me/drive/models.py
@@ -1,7 +1,7 @@
-import os
+# import os
 from django.db import models
 from django.utils.safestring import mark_safe
-from share_with_me.settings import MEDIA_ROOT, MEDIA_URL
+from share_with_me.settings import MEDIA_ROOT
 
 
 # Create your models here.
@@ -10,17 +10,11 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to=MEDIA_ROOT, blank=True)
 
-    def url(self):
-        image_name = os.path.basename(str(self.image))
-        a = os.path.join(MEDIA_URL, image_name)
-        print('AAAAAAAAAAAAAAAAAAAAAAA', a)
-        return a
-
     def image_tag(self):
         # used in the admin site model as a "thumbnail"
-        return mark_safe('<img src="{}" width="150" height="150" />'.format(self.url()))
-    image_tag.short_description = 'Image'
+        return mark_safe('<img src="{}" width="250" height="150" />'.format(self.image.url))
+    image_tag.short_description = 'ImageTag'
     image_tag.allow_tags = True
 
     def __str__(self):
-        return 'Image idk'
+        return str(self.image)
