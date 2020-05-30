6cac72ec676804e1ac6ea08bf444a6be88cdfb6f
Hristiyana <hpmalinova@github.com>
2020-05-30 16:02:16 +0300

    Add views structure

---
+++ share_with_me/drive/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+
+from drive.views import index, images
+
+app_name = 'drive'
+
+images_patterns = [
+    path('', images.list, name='list'),
+    path('<int:image_id>/', images.detail, name='detail'),
+    # path('new/', images.CourseCreateView.as_view(), name='create'),
+    # path('new-experiment/', images.new_experiment, name='new-experiment'),
+    # path('update/<int:course_id>/', images.edit_course, name='update'),
+    # path('delete/<int:course_id>/', images.delete_course, name='delete'),
+    # path('session/', images.session, name='session'),
+]
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('images/', include((images_patterns, 'images')))
+]
---
+++ share_with_me/drive/views/images.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from drive.models import Image
+
+
+def list(request):
+    return render(request, 'images/list.html', {'images': Image.objects.all()})
+
+
+def detail(request, image_id):
+    image = get_object_or_404(Image, id=image_id)
+    return render(request, 'images/detail.html', {'image': image})
+
+
+# class ImageCreateView(CreateView):
+#     model = Course
+#     fields = ['name', 'description', 'start_date', 'end_date']
+#     template_name = 'courses/create.html'
+
+#     def get_success_url(self, **kwargs):
+#         return reverse_lazy('education:courses:detail', kwargs={'course_id': self.object.id})
