3a6db42e53be9402d0d7303e19235ecda70d4e7a
TsvetomirTsvetkov <cvetomircvetkov24@gmail.com>
2020-05-30 15:02:50 +0300

    Fixed Admin Template

--- share_with_me/drive/models.py
+++ share_with_me/drive/models.py
@@ -1,22 +1,26 @@
-from django.core.files.storage import FileSystemStorage
+import os
 from django.db import models
-# from django.utils.html import mark_safe
+from django.utils.safestring import mark_safe
+from share_with_me.settings import MEDIA_ROOT, MEDIA_URL
 
 
-fs = FileSystemStorage(location='/media/shared_files')
+# Create your models here.
+class Image(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to=MEDIA_ROOT, blank=True)
 
+    def url(self):
+        image_name = os.path.basename(str(self.image))
+        a = os.path.join(MEDIA_URL, image_name)
+        print('AAAAAAAAAAAAAAAAAAAAAAA', a)
+        return a
 
-class Picture(models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    data = models.ImageField(storage=fs)
+    def image_tag(self):
+        # used in the admin site model as a "thumbnail"
+        return mark_safe('<img src="{}" width="150" height="150" />'.format(self.image.url))
+    image_tag.short_description = 'ImageTag'
+    image_tag.allow_tags = True
 
     def __str__(self):
-        return self.name
-
-
-'''
-    def image_tag(self):
-        return mark_safe('<img src="/media/shared_files/%s" width="150" height="150" />' % (self.name))
-
-    image_tag.short_description = 'Image'
-'''
+        return str(self.image)
--- share_with_me/share_with_me/urls.py
+++ share_with_me/share_with_me/urls.py
@@ -1,5 +1,4 @@
 """share_with_me URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -16,6 +15,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
