75b09b428d439c085384206fb29884360cb2e4e4
TsvetomirTsvetkov <cvetomircvetkov24@gmail.com>
2020-05-31 13:09:46 +0300

    Fixed FileDownload

--- share_with_me/drive/admin.py
+++ share_with_me/drive/admin.py
@@ -5,5 +5,5 @@ from drive.models import Image
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     # explicitly reference fields to be shown, note image_tag is read-only
-    list_display = ('image_tag', 'description', 'image',)
+    list_display = ('image_tag', 'description', 'image', 'file')
     readonly_fields = ('image_tag',)
