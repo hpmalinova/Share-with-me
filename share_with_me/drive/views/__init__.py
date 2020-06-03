427da127bcf780edea0a20fac0ceb61d46611bf6
Hristiyana <hpmalinova@github.com>
2020-06-03 17:33:20 +0300

    Add counter

--- share_with_me/drive/views/__init__.py
+++ share_with_me/drive/views/__init__.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from drive.models import Courses, Image
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    # values_list('course', flat=True).distinct().
+    num_subjects = Courses.objects.all().count()
+    num_files = Image.objects.all().count()
+
+    context = {
+        'num_subjects': num_subjects,
+        'num_files': num_files,
+        'num_visits': num_visits,
+    }
+    return render(request, 'index.html', context=context)
 
 import drive.views.images
